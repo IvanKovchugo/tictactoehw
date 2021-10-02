92cdebbb8b6655ef836a51019b85b6a4a874ae2c
IvanKovchugo <91083296+IvanKovchugo@users.noreply.github.com>
2021-10-02 14:27:45 +0300

    Initial commit

---
+++ src/TICTACTOE/Main.java
@@ -0,0 +1,12 @@
+package TICTACTOE;
+
+public class Main {
+    private static char map[][];
+    private static final int SIZE = 3;
+    private static final int DOTS_TO_WIN = 3;
+    private static final char EL_X
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
